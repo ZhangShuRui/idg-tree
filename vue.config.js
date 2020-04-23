b1876f3bc79a96fc0895e66f40016d26a8ad814d
zhangyi <zhangyi@corp-ci.com>
2020-04-23 16:33:24 +0800

    cli config 更新

--- vue.config.js
+++ vue.config.js
@@ -1,95 +1,7 @@
 const apiMocker = require('@idg/mocker')
 const path = require('path')
 const globby = require('globby')
-const transpileDependencies = [
-  '@idg/idg',
-  /* '@idg/account',
-  '@idg/ucenter',
-  '@idg/acl', */
-]
-const chainWebpack = config => {
-  config
-    .externals({
-      'vue': {
-        root: 'Vue',
-        commonjs: 'vue',
-        commonjs2: 'vue',
-        amd: 'vue'
-      },
-      'vue-router': {
-        root: 'VueRouter',
-        commonjs: 'vue-router',
-        commonjs2: 'vue-router',
-        amd: 'vue-router'
-      },
-      'vuex': {
-        root: 'Vuex',
-        commonjs: 'vuex',
-        commonjs2: 'vuex',
-        amd: 'vuex'
-      },
-      'vue-i18n': {
-        root: 'VueI18n',
-        commonjs: 'vue-i18n',
-        commonjs2: 'vue-i18n',
-        amd: 'vue-i18n'
-      },
-      'axios': {
-        root: 'axios',
-        commonjs: 'axios',
-        commonjs2: 'axios',
-        amd: 'axios'
-      },
-      'lodash': {
-        root: '_',
-        commonjs: 'lodash',
-        commonjs2: 'lodash',
-        amd: 'lodash'
-      },
-      'qs': {
-        root: 'Qs',
-        commonjs: 'qs',
-        commonjs2: 'qs',
-        amd: 'qs'
-      },
-      'localforage': {
-        root: 'localforage',
-        commonjs: 'localforage',
-        commonjs2: 'localforage',
-        amd: 'localforage'
-      },
-      '@idg/iview': {
-        root: 'iview',
-        commonjs: '@idg/iview',
-        commonjs2: '@idg/iview',
-        amd: '@idg/iview'
-      },
-      '@idg/idg': {
-        root: 'Idg',
-        commonjs: '@idg/idg',
-        commonjs2: '@idg/idg',
-        amd: '@idg/idg'
-      },
-      /* '@idg/account': {
-        root: 'IdgAccount',
-        commonjs: '@idg/account',
-        commonjs2: '@idg/account',
-        amd: '@idg/account'
-      },
-      '@idg/ucenter': {
-        root: 'IdgUcenter',
-        commonjs: '@idg/ucenter',
-        commonjs2: '@idg/ucenter',
-        amd: '@idg/ucenter'
-      },
-      '@idg/acl': {
-        root: 'IdgAcl',
-        commonjs: '@idg/acl',
-        commonjs2: '@idg/acl',
-        amd: '@idg/acl'
-      } */
-    })
-}
+const { transpileDependencies, chainWebpack } = require('@idg/cli-config')
 
 const devConfig = {
   transpileDependencies,
@@ -112,6 +24,11 @@ const devConfig = {
 }
 
 const prodConfig = {
+  chainWebpack (cfg) {
+    chainWebpack(cfg, {
+      addExternals: true
+    })
+  },
   transpileDependencies,
   css: {
     loaderOptions: {
